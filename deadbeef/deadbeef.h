4ec1d9bef69116ea58f49178562b04f7c5376e0b
Andres Rodriguez <josh.rodriguez.b@gmail.com>
2018-12-08 23:20:11 +0100

    Cypher working Header

--- deadbeef/deadbeef.h
+++ deadbeef/deadbeef.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 
-int cipher32b_WORD(uintptr_t input, std::string key, int& output);
+void cipher32b_WORD(uintptr_t input, std::string key, std::string& output);
+void decipher32b_WORD(std::string input, std::string key, std::string& output);
 void stream2hex(const std::string str, std::string& hexstr, bool capital);
 void hex2stream(std::string hexstr, std::string& str);
+std::string strBin2hex(std::string strBinary);
